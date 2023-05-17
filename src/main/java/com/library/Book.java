71347dc07d9785f8b5f08c1422773639a1a02c68
mint723 <neu990112@gmail.com>
2023-05-17 15:30:47 +0900

    BooksArrays class 제거 후 기존 메서드를 Book class로, ArrayList 변수를 start class로 이동

--- src/main/java/com/library/Book.java
+++ src/main/java/com/library/Book.java
@@ -1,10 +1,13 @@
 package main.java.com.library;
 
+import java.util.Scanner;
+
 public class Book {
     static int count;
     int bookNo;
     String bookName;
     Boolean canRental;
+    static Scanner sc = new Scanner(System.in);
 
     public Book(String name){
         this.bookNo = ++count;
@@ -17,6 +20,9 @@ public class Book {
     public void setBookName(String bookName) {
         this.bookName = bookName;
     }
+    public void setBookNo(int bookNo){
+        this.bookNo = bookNo;
+    }
     public int getBookNo() {
         return bookNo;
     }
@@ -25,6 +31,39 @@ public class Book {
     }
     public void setCanRental(Boolean canRental) {
         this.canRental = canRental;
+    }
+
+    public static void bookList() {
+        System.out.printf("%n------Book List------%n");
+        for (int i = 1; i < Start.bookCase.size()+1; i++) {
+            bookInfo(i);
+            System.out.println("---------------------");
+        }
+        System.out.println("---------------------");
+    }
+
+    public static void bookInfo(int index) {
+        System.out.println("책 이름 : "+Start.bookCase.get(index-1).getBookName());
+        System.out.println("책 번호 : "+Start.bookCase.get(index-1).getBookNo());
+        System.out.println("책 대출 가능 여부 : "+Start.bookCase.get(index-1).getCanRental());
+    }
+
+    public static void addBook() {
+        System.out.println("등록하실 책의 이름을 입력해주세요");
+        String book = sc.nextLine();
+        Start.bookCase.add(new Book(book));
+    }
+
+    public static void deleteBook() {
+        System.out.println("삭제하실 책의 번호를 입력해주세요");
+        int index = sc.nextInt();
+        for (int i = 0; i < Start.bookCase.size(); i++) {
+            if((index)==Start.bookCase.get(i).bookNo) {
+                System.out.printf("%d번 책 (%s)이 삭제되었습니다.%n",Start.bookCase.get(i).bookNo,Start.bookCase.get(i).bookName);
+                Start.bookCase.remove(i);
+                return;
+            }
+        }
     }
 
 }
--- src/main/java/com/library/Start.java
+++ src/main/java/com/library/Start.java
@@ -1,22 +1,25 @@
 package main.java.com.library;
 
+import java.util.ArrayList;
 import java.util.Scanner;
-import static main.java.com.library.BooksArrays.*;
+import static main.java.com.library.Book.*;
 import static main.java.com.library.Search.search;
 import static main.java.com.library.RentalAndReturn.rentalAndRetrun;
+import static main.java.com.library.ChangeInfo.changeInfo;
 public class Start {
+    public static ArrayList<Book> bookCase = new ArrayList<>();
     static Scanner sc = new Scanner(System.in);
     static int select=0;
-    static String book = "";
     static public void start() {
-        while(select != 6) {
+        while(select != 7) {
             System.out.println("******************************");
             System.out.printf("%17s%n","*1.도서 등록*");
             System.out.printf("%17s%n","*2.도서 삭제*");
             System.out.printf("%17s%n","*3.도서 검색*");
             System.out.printf("%17s%n","*4.도서 목록*");
             System.out.printf("%17s%n","*5.도서 대여 및 반납*");
-            System.out.printf("%17s%n","*6.종료*");
+            System.out.printf("%17s%n","*6.도서 정보 변경*");
+            System.out.printf("%17s%n","*7.종료*");
             System.out.println("******************************");
             select = sc.nextInt();
             switch (select) {
@@ -36,6 +39,9 @@ public class Start {
                     rentalAndRetrun();
                     break;
                 case 6:
+                    changeInfo();
+                    break;
+                case 7:
                     System.out.println("프로그램을 종료합니다.");
                     break;
                 default:
