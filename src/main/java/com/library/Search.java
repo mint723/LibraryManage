5dd960a1d7d51457a19fc55b4f75165a55dea06f
mint723 <neu990112@gmail.com>
2023-05-17 15:43:17 +0900

    객체 sc 삭제 후 start.sc 사용으로 통일 및 참조변수 bookCase를 Start class에서 불러오도록 수정

--- src/main/java/com/library/Search.java
+++ src/main/java/com/library/Search.java
@@ -1,23 +1,21 @@
 package main.java.com.library;
 
-import java.util.Scanner;
-
 public class Search {
-    static Scanner sc = new Scanner(System.in);
 
     public static void search(){
         int count = 0;
         System.out.printf("찾으시는 책을 검색해주세요%n검색 : ");
-        String searchWord = sc.nextLine();
+        Start.sc.nextLine();
+        String searchWord = Start.sc.nextLine();
         searchWord = searchWord.replaceAll("\\s", "");
-        for (int i = 0; i < BooksArrays.bookCase.size(); i++) {
-            String book = BooksArrays.bookCase.get(i).bookName;
+        for (int i = 0; i < Start.bookCase.size(); i++) {
+            String book = Start.bookCase.get(i).bookName;
             book = book.replaceAll("\\s", "");
             if (searchWord.equals("")) {}
             else {
                 for (int j = 0; j < book.length()-searchWord.length()+1; j++) {
                     if(book.substring(j, searchWord.length()+j).equals(searchWord)) {
-                        BooksArrays.bookInfo(i+1);
+                        Book.bookInfo(i+1);
                         count++;
                         break;
                     }
