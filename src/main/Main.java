c594dccc21d7d2986541bcfecabff34557de4588
mint723 <neu990112@gmail.com>
2023-05-17 15:46:28 +0900

    none

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,9 +1,10 @@
 package main;
-import static main.java.com.library.Start.start;;
+import static main.java.com.library.Start.start;
 public class Main {
     public static void main(String[] args) {
 
         start();
 
     }
+
 }
